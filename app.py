d723adb8ac51929e26ab899784fab9bf72765624
Thiago Cacio <94044199+NihilLeaf@users.noreply.github.com>
2022-09-01 19:05:19 -0300

    Funcionalidade adicionada, inicio de estilo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import imp
+from cgitb import text
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -10,11 +10,24 @@ class SayHello(App):
     def build(self):
         self.window = GridLayout()
         self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
 
+        self.window.add_widget(Image(source=('logo.jpg')))
 
-        self.window.add_widget(Image(sourse('logo.jpg')))
+        self.greeting = Label(text = 'What is your name?')
+        self.window.add_widget(self.greeting)
+        self.user = TextInput(multiline=False)
+        self.window.add_widget(self.user)
+        self.button = Button(text='GREET')
+        self.button.bind(on_press=self.callback)
+        self.window.add_widget(self.button)
 
         return self.window
+    
+    def callback(self, event):
+        self.greeting.text = f'Hey, {self.user.text}! Welcome.'
+
 
 if __name__ == '__main__':
     SayHello().run()
